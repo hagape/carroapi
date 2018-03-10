f8f1af7cd58bf3f6d0007e8c1e83b4c896440411
logonrm <logonrm@fiap.com.br>
2018-03-10 14:02:55 -0300

    inclusão da url da imagem do carro

--- src/main/kotlin/br/com/marli/carroapi/entity/Carro.kt
+++ src/main/kotlin/br/com/marli/carroapi/entity/Carro.kt
@@ -1,13 +1,15 @@
 package br.com.marli.carroapi.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.awt.Image
 
 @Document
 data class Carro(@Id var id: String?,
                  var marca: String,
                  var modelo: String,
                  var ano: Int,
-                 var placa: String){
+                 var placa: String,
+                 var urlImagem: String?){
 /*
     constructor(): this(id: "",
      marca:"",
