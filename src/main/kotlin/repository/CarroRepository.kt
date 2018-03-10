52a8c06fd2a68820c4aa39c2de8e248723a0d92c
logonrm <logonrm@fiap.com.br>
2018-03-10 12:24:52 -0300

    Aula Android - Carro

---
+++ src/main/kotlin/repository/CarroRepository.kt
@@ -0,0 +1,16 @@
+package br.com.marli.carroapi.repository
+
+import br.com.marli.carroapi.entity.Carro
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+
+@Repository
+interface  CarroRepository : MongoRepository<Carro, String>{
+
+    fun findByAno(ano : Int): List<Carro>
+
+    fun findByPlaca(placa: String): List<Carro>
+
+    fun findByMarcaContaining(marca : String): List<Carro>
+}
